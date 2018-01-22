d669c163b7895dd568f29cd8968f47caee56ad30
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-22 16:29:43 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int less, high, i, flag;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &less, &high);
+
+    printf("Prime numbers between %d and %d are: ", less, high);
+
+    while (less < high)
+    {
+        flag = 0;
+
+        for(i = 2; i <= low/2; ++i)
+        {
+            if(low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", low);
+
+        ++low;
+    }
+
+    return 0;
