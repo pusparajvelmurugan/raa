2496c6c6de2fccaab20c4ba125a70b853cc1f39f
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-22 16:34:19 +0530

    Create prim.c

---
+++ prim.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int less, high, i, flag;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &less, &high);
+
+    printf("Prime numbers between %d and %d are: ", less, high);
+
+    while (less < high)
+    {
+        flag = 0;
+
+        for(i = 2; i <= low/2; ++i)
+        {
+            if(low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        return 0;
+    }
