abc8b1c6e407e9f8cc933a5ef42e8f4aea448e5b
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-22 16:58:45 +0530

    Create ca.c

---
+++ ca.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+int main()
+{
+int a,i,b;
+ printf("enter the number");
+ scanf("%d",&a);
+ b=a*5;
+ for(i=0;i<=5;i++)
+  printf("print first five multiple values %d",a);
+ return 0;
+}
