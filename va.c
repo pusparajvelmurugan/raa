1702709f3f67d50c2b548437c2fb6a49355c7cc2
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-22 16:52:51 +0530

    Create va.c

---
+++ va.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int a, b, fact = 1;
+printf("Enter a number\n");
+scanf("%d", &a);
+for (c = 1; c <= n; c++)
+fact = fact * c;
+printf("Factorial of %d = %d\n", b, fact);
+ return 0;
+}
