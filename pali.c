89e40e70ad1739dc8d85f97adabbf3004be6536d
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-22 16:08:46 +0530

    Create pali.c

---
+++ pali.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,sum;
+    printf("enter the value is");
+    scanf("%d",&a);
+    sum=a%10;
+    a/10;
+    printf("enter the number is palindrome");
+    }
+    else{
+        printf("enter the number is not palindrome");
+        return 0;
+    }
+    
+}
