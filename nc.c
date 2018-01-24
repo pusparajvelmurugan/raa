08343a11d2cbaec90e984e09fdf88d4a13a899bf
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-24 16:13:48 +0530

    Create nc.c

---
+++ nc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int sub;
+    double a=4.50;
+    double b=5.50;
+    printf("enter the hours and min is");
+    scanf("%d",&a);
+    printf("enter the hour and min is");
+    scanf("%d",&b);{
+  sub=(a-b);
+  printf("\n subract the  hours and min value is %d",sub);
+  return 0;
+    }
+}
