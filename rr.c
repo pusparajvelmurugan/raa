726e36119de1c2fdc90b9aa5d849b6648cc5095e
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-31 16:04:18 +0530

    Create rr.c

---
+++ rr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[20];
+clrscr();
+printf("enter the numbers:");
+scanf("%c",&a);
+if(string==a)
+{
+printf("\n given string is numeric:");
+}
+else{
+printf("\n given string is not numeric:");
+}
+getch();
+}
