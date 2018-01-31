a2b48b7af70f383876ac6d3db5c94a87612df897
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-31 16:20:56 +0530

    Create rrr.c

---
+++ rrr.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int num,num1,num2,num3;
+char a[];
+printf("enter the numbers");
+scanf("%d",&num);
+num1<num2?num1<num3?printf("num1 is small"):printf("num2 is small")num2<num3?printf("num2 is small");
+getch();
+}
+
